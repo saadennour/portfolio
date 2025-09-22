c610063f719aa9deaf9ac3e7ae277bdf72894273
saad-ennour <saadennourfarhan@gmail.com>
2025-09-22 13:53:30 +0100

    Updates

---
+++ _next/static/chunks/app/project/[slug]/page-da63a95bc0825485.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144],{3249:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.t.bind(n,4839,23)),Promise.resolve().then(n.t.bind(n,8157,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[485,512,441,517,358],()=>s(3249)),_N_E=e.O()}]);
